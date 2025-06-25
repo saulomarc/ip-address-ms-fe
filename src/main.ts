5e7a3cdf210ca10dacd081efcd7495c6e4c8d09b
saulomarc <mcsaulo@up.edu.ph>
2025-06-25 14:46:15 +0800

    chore: add pinia persisted state

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Notifications from 'notiwind'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedState)
+app.use(pinia)
 app.use(router)
 app.use(Notifications)
 
