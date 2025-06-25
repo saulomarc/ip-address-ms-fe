7848c4552948ccdf41aae86964ef190242392c8b
saulomarc <mcsaulo@up.edu.ph>
2025-06-25 14:47:30 +0800

    chore: add role and permission type in user

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,24 @@
 export interface User {
     name: string,
     email: string,
+    roles?: Role[],
+    permissions?: Permission[]
     imageUrl?: string
+}
+
+export interface Role {
+    id: number,
+    name: string, 
+    guard_name: string,
+    created_at: Date,
+    updated_at: Date,
+}
+
+
+export interface Permission {
+    id: number,
+    name: string, 
+    guard_name: string,
+    created_at: Date,
+    updated_at: Date,
 }
