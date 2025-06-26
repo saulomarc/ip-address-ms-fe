23e41c58ad5ed92ddb8ee09b2dbee6085f600a0c
saulomarc <mcsaulo@up.edu.ph>
2025-06-26 16:34:21 +0800

    ui: integrate vue final modal and create custom modal component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './assets/main.css'
+import 'vue-final-modal/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVfm } from 'vue-final-modal'
 import Notifications from 'notiwind'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
@@ -10,10 +12,12 @@ import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
+const vfm = createVfm()
 
 pinia.use(piniaPluginPersistedState)
 app.use(pinia)
 app.use(router)
 app.use(Notifications)
+app.use(vfm)
 
 app.mount('#app')
