005e08bd4dfa3e3a2fbdd3f60b1d96a15f245ae0
saulomarc <mcsaulo@up.edu.ph>
2025-06-26 23:07:22 +0800

    ui: create drawer component

---
+++ src/components/Drawer.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <TransitionRoot as="template" :show="isOpen">
+            <Dialog class="relative z-10" @close="closeDrawer">
+                <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
+                    <div class="fixed inset-0 bg-gray-500/75 transition-opacity" />
+                </TransitionChild>
+                
+                <div class="fixed inset-0 overflow-hidden">
+                    <div class="absolute inset-0 overflow-hidden">
+                        <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+                            <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
+                                <DialogPanel class="pointer-events-auto w-screen max-w-md">
+                                    <div class="flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl">
+                                        <div class="px-4 sm:px-6">
+                                            <div class="flex items-start justify-between">
+                                                <DialogTitle class="text-base font-semibold text-gray-900">Panel title</DialogTitle>
+                                                <div class="ml-3 flex h-7 items-center">
+                                                    <button type="button" class="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 focus-visible:outline-hidden" @click="closeDrawer">
+                                                        <span class="absolute -inset-2.5" />
+                                                        <span class="sr-only">Close panel</span>
+                                                        <XMarkIcon class="size-6" aria-hidden="true" />
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="relative mt-6 flex-1 px-4 sm:px-6">
+                                            <slot name="content">
+
+                                            </slot>
+                                        </div>
+                                    </div>
+                                </DialogPanel>
+                            </TransitionChild>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+
+const props = defineProps<{
+    isOpen: boolean
+}>()
+
+const emit = defineEmits<{
+    closeDrawer: []
+}>()
+
+function closeDrawer() {
+    emit('closeDrawer')
+}
+</script>
