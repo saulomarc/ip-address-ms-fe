2a06b42885fc9c81b961dbaf5cdff8330362dbf8
saulomarc <mcsaulo@up.edu.ph>
2025-06-26 23:07:48 +0800

    ui: update unauthorized and not-found page design

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen text-center p-4">
-        <h1 class="text-4xl font-bold text-red-600 mb-4">404 - Not Found</h1>
-        <p class="text-lg text-gray-700 mb-6">
-            This resource does not exist.
-        </p>
-        <router-link to="/" class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded">
-            Go Home
-        </router-link>
-    </div>
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="text-center">
+            <p class="text-base font-semibold text-indigo-600">404</p>
+            <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Page not found</h1>
+            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Sorry, we couldn't find the page you're looking for.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+                <router-link to="/" class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded">
+                    Go Back Home
+                </router-link>
+            </div>
+        </div>
+    </main>
 </template>
