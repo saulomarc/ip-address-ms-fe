14070156d1edd52ecbc8726c22540c1200b7ccbb
saulomarc <mcsaulo@up.edu.ph>
2025-06-26 16:37:57 +0800

    chore: remove HelloWorld import from app vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue'
 import Alert from './components/Alert.vue'
 import DashboardLayout from './layouts/DashboardLayout.vue';
 import PlainLayout from './layouts/PlainLayout.vue';
@@ -16,7 +15,7 @@ const layout = computed(() => {
 
 <template>
   <Alert />
-
+  
   <component :is="layout">
   </component>
 </template>
